49b895caa5c774b04e1aa6e10e6e97e6d06cc861
GUchechi <godswilluchendu7@gmail.com>
2022-09-06 16:28:36 +0100

    sorting and ordering via likes

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 const container = document.querySelector(".blogs")
 
 const renderPosts = async () => {
-    let url =  'http://localhost:3000/posts'
+    let url =  'http://localhost:3000/posts?_sort=likes&_order=desc'
 
     const res = await fetch(url)
     const posts = await res.json()
