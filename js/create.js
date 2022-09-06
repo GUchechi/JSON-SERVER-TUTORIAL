b0a7229ed133d7fe057e8b73676d872acca25ccd
GUchechi <godswilluchendu7@gmail.com>
2022-09-06 17:05:14 +0100

    added post requests

--- js/create.js
+++ js/create.js
@@ -1,1 +1,23 @@
 // javascript for create.html
+const form = document.querySelector('form');
+
+const createPost = async (e) => {
+    e.preventDefault();
+
+    const docs = {
+        title: form.title.value,
+        body: form.body.value,
+        likes: 0
+    }
+
+    await fetch('http://localhost:3000/posts', {
+        method: 'POST',
+        body: JSON.stringify(docs),
+        headers: { 'Content-type': 'application/json' }
+    });
+
+    window.location.replace("/index.html");
+    
+}
+
+form.addEventListener('submit', createPost)
