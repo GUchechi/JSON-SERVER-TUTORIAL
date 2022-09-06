c1f5e2666c1af1f60e845c8df2c437fb132fe365
GUchechi <godswilluchendu7@gmail.com>
2022-09-06 15:03:06 +0100

    added template strings

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
 // javascript for index.html
+
+const renderPosts = async () => {
+    let url =  'http://localhost:3000/posts'
+
+    const res = await fetch(url)
+    const posts = await res.json()
+
+    let template = '';
+    posts.forEach(post => {
+        template += `
+            <div class="post">
+                <h2>${post.title}</h2>
+                <p><small>${post.likes}</small></p>
+                <p>${post.body.slice(0, 100)}</p>
+                <a href="/detail.html">Read more...</a>
+            </div>
+        `
+    })
+    
+}
+
+
+window.addEventListener('DOMContentLoaded', () => renderPosts());
